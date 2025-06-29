8237f0550881c51c662043b04348caded8a16cf9
ksaini1108 <konica.saini@ibm.com>
2025-06-29 14:15:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-south"
+}
+
+variable "resource_group" {
+  default = "Default"
+}
+
+variable "cos_instance_name" {
+  default = "tf-cos-instance"
+}
+
+variable "bucket_name" {
+  default = "tf-bucket-demo-123456"
+}
+
+variable "bucket_region" {
+  default = "us-south"
+}
